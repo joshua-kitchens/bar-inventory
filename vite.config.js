54b4dd92a107733dc67ec857a27a791f3c6c8038
Josh Kitchens <josh@jkitchens.dev>
2026-05-12 21:46:56 -0400

    updates for sub domain hosting

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Change this to match your GitHub repo name if it differs from 'bar-inventory'
 export default defineConfig({
   plugins: [react()],
-  base: '/bar-inventory/',
+  base: '/',
 })
